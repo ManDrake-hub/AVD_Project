513c6273b49e182ff655076e189bf1f82a937ac1
giosullutrone <giovisull@gmail.com>
2023-05-03 10:11:21 +0200

    Moved get_sensors functionality inside sensors.py

--- team_code/carla_behavior_agent/misc.py
+++ team_code/carla_behavior_agent/misc.py
@@ -103,18 +103,6 @@ def is_within_distance(target_transform, reference_transform, max_distance, angl
 
     return min_angle < angle < max_angle
 
-"""
-θ = atan2(b x a, a · b),
-
-where θ is the signed angle between the vectors, a and b are the two vectors, x denotes the cross product of two vectors, · denotes the dot product of two vectors, and atan2 is a function that returns the angle in radians between -π and π.
-
-In this formula, the cross product of two vectors a and b is a scalar given by a x b = |a| |b| sin(θ), where θ is the angle between the vectors. The sign of the cross product indicates the orientation of the vectors relative to each other.
-
-So, if the z-component of the cross product a x b is positive, then the angle between the vectors is counterclockwise, and if it is negative, then the angle is clockwise.
-
-The angle is signed, which means that it can be positive or negative. A positive angle indicates a counterclockwise rotation from the first vector to the second vector, while a negative angle indicates a clockwise rotation.
-"""
-
 def compute_magnitude_angle_with_sign(target_location, current_location, orientation):
     """
     Compute relative angle and distance between a target_location and a current_location
@@ -131,113 +119,8 @@ def compute_magnitude_angle_with_sign(target_location, current_location, orienta
     d_angle = math.degrees(math.atan2(np.cross(forward_vector, target_vector), np.dot(forward_vector, target_vector)))
     return (norm_target, d_angle)
 
-def get_sensors(ego_vehicle, vehicle_list, sensors, max_distance=60):
-    """
-        :param ego_vehicle: ego vehicle
-        :param vehicle_list: list of actors (not limited to vehicles)
-        :param sensors: list of tuples formatted as (sensor_id: str, range_min: float, range_max: float, angle_min: float, angle_max: float)
-        :return: a dict with sensor_id as key and list of tuples formatted as (actor, distance, angle) as values
-    """
-    ego_vehicle_loc = ego_vehicle.get_location()
-    ego_vehicle_transform = ego_vehicle.get_transform()
-
-    def dist(v): return v.get_location().distance(ego_vehicle_loc)
-    vehicles = [(x, *compute_magnitude_angle_with_sign(x.get_location(), ego_vehicle_loc, ego_vehicle_transform.rotation.yaw)) for x in vehicle_list 
-                if ((dist(x) < max_distance) if max_distance is not None else True) and not is_hero(x)]
-    vehicles.sort(key=lambda x: x[1])
-
-    sensors_result = {}
-    for sensor in sensors:
-        sensor_id, sensor_range_min, sensor_range_max, sensor_angle_min, sensor_angle_max = sensor
-        sensors_result[sensor_id] = []
-        for v in vehicles:
-            vehicle, distance, angle = v
-            if (sensor_range_min <= distance <= sensor_range_max and sensor_angle_min <= angle <= sensor_angle_max):
-                sensors_result[sensor_id].append((vehicle, distance, angle))
-    return sensors_result
-
 def is_hero(vehicle):
     return 'role_name' in vehicle.attributes and 'hero' in vehicle.attributes['role_name']
-
-def get_sensors_locations(ego_vehicle, location_list, sensors, max_distance=60):
-    """
-        :param ego_vehicle: ego vehicle
-        :param location_list: list of tuples formatted as (actor, location)
-        :param sensors: list of tuples formatted as (sensor_id: str, range_min: float, range_max: float, angle_min: float, angle_max: float)
-        :return: a dict with sensor_id as key and list of tuples formatted as (actor, distance, angle) as values
-    """
-    ego_vehicle_loc = ego_vehicle.get_location()
-    ego_vehicle_transform = ego_vehicle.get_transform()
-
-    def dist(location): return location.distance(ego_vehicle_loc)
-    vehicles = [(vehicle, *compute_magnitude_angle_with_sign(location, ego_vehicle_loc, ego_vehicle_transform.rotation.yaw)) for vehicle, location in location_list 
-                if ((dist(location) < max_distance) if max_distance is not None else True) and not is_hero(vehicle)]
-    vehicles.sort(key=lambda x: x[1])
-
-    sensors_result = {}
-    for sensor in sensors:
-        sensor_id, sensor_range_min, sensor_range_max, sensor_angle_min, sensor_angle_max = sensor
-        sensors_result[sensor_id] = []
-        for v in vehicles:
-            vehicle, distance, angle = v
-            if (sensor_range_min <= distance <= sensor_range_max and sensor_angle_min <= angle <= sensor_angle_max):
-                sensors_result[sensor_id].append((vehicle, distance, angle))
-    return sensors_result
-
-def get_sensors_ga(ego_vehicle, vehicle_list, sensors, max_distance=60, max_vehicles=2):
-    # sensors: [ (id, range_min, range_max, angle_min, angle_max) ]
-    # out:     {sensor_id: {"active", "distances", "angles", "speed_deltas"}}
-    ego_vehicle_loc = ego_vehicle.get_location()
-    ego_vehicle_transform = ego_vehicle.get_transform()
-
-    def dist(v): return v.get_location().distance(ego_vehicle_loc)
-    vehicles = [(x, *compute_magnitude_angle_with_sign(x.get_location(), ego_vehicle_loc, ego_vehicle_transform.rotation.yaw)) for x in vehicle_list 
-                if dist(x) < max_distance and 
-                (not 'role_name' in x.attributes or ('role_name' in x.attributes and not 'hero' in x.attributes['role_name']))]
-    vehicles.sort(key=lambda x: x[1])
-
-    sensors_result = {}
-    for sensor in sensors:
-        sensor_id, sensor_range_min, sensor_range_max, sensor_angle_min, sensor_angle_max = sensor
-        sensors_result[sensor_id] = {"active": False, "distances": [], "angles": [], "speed_deltas": []}
-        for v in vehicles:
-            vehicle, distance, angle = v
-            if (sensor_range_min <= distance <= sensor_range_max and sensor_angle_min <= angle <= sensor_angle_max):
-                if len(sensors_result[sensor_id]["distances"]) >= max_vehicles:
-                    break
-                sensors_result[sensor_id]["active"] = True
-                sensors_result[sensor_id]["distances"].append((distance - sensor_range_min) / (sensor_range_max - sensor_range_min)) # TODO: change
-                sensors_result[sensor_id]["angles"].append((angle - sensor_angle_min) / (sensor_angle_max - sensor_angle_min))
-                sensors_result[sensor_id]["speed_deltas"].append((vehicle.get_velocity().length() / (ego_vehicle.get_velocity().length() + 1e-5)) - 1.0)
-                # sensors_result[sensor_id]["dunno"].append((vehicle.get_velocity().length() / (self._vehicle.get_velocity().length() + 1e-5)) - 1.0)
-    return sensors_result
-
-def get_sensors_ga(self, ego_vehicle_loc, ego_vehicle_transform, vehicle_list, sensors, max_distance=30, max_vehicles=2):
-    # sensors: [ (id, range_min, range_max, angle_min, angle_max) ]
-    # out:     {sensor_id: {"active", "distances", "angles", "speed_deltas"}}
-
-    def dist(v): return v.get_location().distance(ego_vehicle_loc)
-    vehicles = [(x, compute_magnitude_angle_with_sign(x.get_location(), ego_vehicle_loc, ego_vehicle_transform.rotation.yaw)) for x in vehicle_list 
-                if dist(x) < max_distance and 
-                (not 'role_name' in x.attributes or ('role_name' in x.attributes and not 'hero' in x.attributes['role_name']))]
-
-    vehicles.sort(key=lambda x: x[1][0])
-    sensors_result = {}
-    for sensor in sensors:
-        sensor_id, sensor_range_min, sensor_range_max, sensor_angle_min, sensor_angle_max = sensor
-        sensors_result[sensor_id] = {"active": False, "distances": [], "angles": [], "speed_deltas": []}
-        for v in vehicles:
-            vehicle, distance_angle = v
-            distance, angle = distance_angle
-            if (sensor_range_min <= distance <= sensor_range_max and sensor_angle_min <= angle <= sensor_angle_max):
-                if len(sensors_result[sensor_id]["distances"]) >= max_vehicles:
-                    break
-                sensors_result[sensor_id]["active"] = True
-                sensors_result[sensor_id]["distances"].append((distance - sensor_range_min) / (sensor_range_max - sensor_range_min)) # TODO: change
-                sensors_result[sensor_id]["angles"].append((angle - sensor_angle_min) / (sensor_angle_max - sensor_angle_min))
-                sensors_result[sensor_id]["speed_deltas"].append((vehicle.get_velocity().length() / (self._vehicle.get_velocity().length() + 1e-5)) - 1.0)
-                # sensors_result[sensor_id]["dunno"].append((vehicle.get_velocity().length() / (self._vehicle.get_velocity().length() + 1e-5)) - 1.0)
-    return sensors_result
 
 def compute_magnitude_angle(target_location, current_location, orientation):
     """
