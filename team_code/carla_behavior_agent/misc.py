69a2ff5e731bd8d0a5f211ea38e2a522a25ea96e
giosullutrone <giovisull@gmail.com>
2023-05-03 09:57:18 +0200

    Fixed bug in get_sensors

--- team_code/carla_behavior_agent/misc.py
+++ team_code/carla_behavior_agent/misc.py
@@ -143,9 +143,36 @@ def get_sensors(ego_vehicle, vehicle_list, sensors, max_distance=60):
 
     def dist(v): return v.get_location().distance(ego_vehicle_loc)
     vehicles = [(x, *compute_magnitude_angle_with_sign(x.get_location(), ego_vehicle_loc, ego_vehicle_transform.rotation.yaw)) for x in vehicle_list 
-                if ((dist(x) < max_distance) if max_distance is not None else True) and 
-                (not 'role_name' in x.attributes or ('role_name' in x.attributes and not 'hero' in x.attributes['role_name']))]
-    vehicles.sort(key=lambda x: x[1][0])
+                if ((dist(x) < max_distance) if max_distance is not None else True) and not is_hero(x)]
+    vehicles.sort(key=lambda x: x[1])
+
+    sensors_result = {}
+    for sensor in sensors:
+        sensor_id, sensor_range_min, sensor_range_max, sensor_angle_min, sensor_angle_max = sensor
+        sensors_result[sensor_id] = []
+        for v in vehicles:
+            vehicle, distance, angle = v
+            if (sensor_range_min <= distance <= sensor_range_max and sensor_angle_min <= angle <= sensor_angle_max):
+                sensors_result[sensor_id].append((vehicle, distance, angle))
+    return sensors_result
+
+def is_hero(vehicle):
+    return 'role_name' in vehicle.attributes and 'hero' in vehicle.attributes['role_name']
+
+def get_sensors_locations(ego_vehicle, location_list, sensors, max_distance=60):
+    """
+        :param ego_vehicle: ego vehicle
+        :param location_list: list of tuples formatted as (actor, location)
+        :param sensors: list of tuples formatted as (sensor_id: str, range_min: float, range_max: float, angle_min: float, angle_max: float)
+        :return: a dict with sensor_id as key and list of tuples formatted as (actor, distance, angle) as values
+    """
+    ego_vehicle_loc = ego_vehicle.get_location()
+    ego_vehicle_transform = ego_vehicle.get_transform()
+
+    def dist(location): return location.distance(ego_vehicle_loc)
+    vehicles = [(vehicle, *compute_magnitude_angle_with_sign(location, ego_vehicle_loc, ego_vehicle_transform.rotation.yaw)) for vehicle, location in location_list 
+                if ((dist(location) < max_distance) if max_distance is not None else True) and not is_hero(vehicle)]
+    vehicles.sort(key=lambda x: x[1])
 
     sensors_result = {}
     for sensor in sensors:
@@ -167,7 +194,7 @@ def get_sensors_ga(ego_vehicle, vehicle_list, sensors, max_distance=60, max_vehi
     vehicles = [(x, *compute_magnitude_angle_with_sign(x.get_location(), ego_vehicle_loc, ego_vehicle_transform.rotation.yaw)) for x in vehicle_list 
                 if dist(x) < max_distance and 
                 (not 'role_name' in x.attributes or ('role_name' in x.attributes and not 'hero' in x.attributes['role_name']))]
-    vehicles.sort(key=lambda x: x[1][0])
+    vehicles.sort(key=lambda x: x[1])
 
     sensors_result = {}
     for sensor in sensors:
@@ -181,7 +208,7 @@ def get_sensors_ga(ego_vehicle, vehicle_list, sensors, max_distance=60, max_vehi
                 sensors_result[sensor_id]["active"] = True
                 sensors_result[sensor_id]["distances"].append((distance - sensor_range_min) / (sensor_range_max - sensor_range_min)) # TODO: change
                 sensors_result[sensor_id]["angles"].append((angle - sensor_angle_min) / (sensor_angle_max - sensor_angle_min))
-                sensors_result[sensor_id]["speed_deltas"].append((vehicle.get_velocity().length() / (self._vehicle.get_velocity().length() + 1e-5)) - 1.0)
+                sensors_result[sensor_id]["speed_deltas"].append((vehicle.get_velocity().length() / (ego_vehicle.get_velocity().length() + 1e-5)) - 1.0)
                 # sensors_result[sensor_id]["dunno"].append((vehicle.get_velocity().length() / (self._vehicle.get_velocity().length() + 1e-5)) - 1.0)
     return sensors_result
 
