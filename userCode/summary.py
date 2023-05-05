f553a78186f2cf5a24e9104000d4ddee2f25cb4f
francesca2311 <f.tirino@studenti.unisa.it>
2023-05-06 00:36:41 +0100

    format

--- userCode/summary.py
+++ userCode/summary.py
@@ -7,8 +7,18 @@ INFRACTIONS = True
 SCORE = True
 SHUTDOWN_EVENT = True
 
+list_necess = []
+merged_dict = {}
+custom_dict = {}
+
+infractions_dict_new = {"collisions_layout":"Coll with Layout", "collisions_pedestrian":"Coll with Pedestrians", "collisions_vehicle":"Coll with Vehicles", 
+                        "red_light":"Red light", "stop_infraction":"Stop", "outside_route_lanes":"Outside lanes", "min_speed_infractions":"Useless Decelerations", 
+                        "yield_emergency_vehicles_infractions":"Yield emergency vehicles", "scenario_timeouts":"Scenario timeouts"}
+score_dict_new = {"score_composed":"Driving score", "score_route":"Route Complention", "score_penalty":"Infraction Penalty"}
+shutdown_event_dict_new = {"route_dev":"Route Deviation", "vehicle_blocked":"Vehicle Blocked", "route_timeout":"Route Timeout"}
+
 # Load the JSON file
-with open('results/routes_avddiem_exam_results.json', 'r') as f:
+with open('results/simulation_results_must.json', 'r') as f:
     data = json.load(f)
 
 # Title
@@ -17,36 +27,47 @@ title = [' ','Global_record']
 routes = data['_checkpoint']['records']
 for i in range(len(routes)):
     title.append(routes[i]['route_id'])
-#Status
+# Status
 status = ['status']
 global_status = data['_checkpoint']['global_record']['status']
 status.append(global_status)
 for i in range(len(routes)):
     status.append(routes[i]['status'])
-#Num infractions
+# Num infractions
 num_infractions = ['num_infractions','']
 for i in range(len(routes)):
     num_infractions.append(routes[i]['num_infractions'])
 
-#Dict infractions global
+# Dict infractions global
 infractions_dict_comp = data['_checkpoint']['global_record']['infractions']
 infractions_dict = dict((k, infractions_dict_comp[k]) for k in list(infractions_dict_comp.keys())[:-3])
 values = [float(val) for val in list(infractions_dict.values())]
+
 # Percentages infractions
 total = sum(values)
 percentages = [val/total*100 for val in values]
 for key in infractions_dict:
-    infractions_dict[key] = f"{percentages.pop(0):.2f}%"
-#Dict score global
+    infractions_dict[key] = f"{round(percentages.pop(0),1):.1f}%"
+# Dict score global
 score_dict = data['_checkpoint']['global_record']['scores_mean']
-#Dict shutdown_event global
+
+# Dict shutdown_event global
 shutdown_event_dict = dict((k, infractions_dict_comp[k]) for k in list(infractions_dict_comp.keys())[-3:])
 
+if INFRACTIONS:
+    list_necess.append(infractions_dict)
+    merged_dict.update(infractions_dict)
+    custom_dict.update(infractions_dict_new)
+if SCORE:
+    list_necess.append(score_dict)
+    merged_dict.update(score_dict)
+    custom_dict.update(score_dict_new)
+if SHUTDOWN_EVENT:
+    list_necess.append(shutdown_event_dict)
+    merged_dict.update(shutdown_event_dict)
+    custom_dict.update(shutdown_event_dict_new)
+
 # Matrix for global
-merged_dict = {}
-merged_dict.update(infractions_dict)
-merged_dict.update(score_dict)
-merged_dict.update(shutdown_event_dict)
 keys = np.array(list(merged_dict.keys()))
 values = np.array(list(merged_dict.values()))
 total_matrix = np.column_stack((keys, values))
@@ -59,29 +80,25 @@ for i in range(len(routes)):
         if total_matrix[j,0] in shutdown_event_dict: 
             shutdown_event_value = data['_checkpoint']['records'][i]['infractions'][total_matrix[j,0]]
             if len(shutdown_event_value) == 0:
-                total_matrix[j][i+2] = None
+                total_matrix[j][i+2] = ''
             else:
                 total_matrix[j][i+2] = shutdown_event_value[0]
         elif total_matrix[j,0] in score_dict:
             score_value = data['_checkpoint']['records'][i]['scores'][total_matrix[j,0]]
-            total_matrix[j][i+2] = score_value
+            total_matrix[j][i+2] = round(score_value,1)
         else:
             infraction_value = len(data['_checkpoint']['records'][i]['infractions'][total_matrix[j,0]])
             if infraction_value == 0:
-                total_matrix[j][i+2] = None
+                total_matrix[j][i+2] = ''
             else:
-                total_matrix[j][i+2] = infraction_value
+                total_matrix[j][i+2] = round(infraction_value,1)
+for row in total_matrix:
+    if row[0] in shutdown_event_dict_new or score_dict_new or infractions_dict_new:
+        row[0] = custom_dict[row[0]]
 print(total_matrix)
 
-''''
-if INFRACTIONS:
-    add()
-if SCORE:
-
-if SHUTDOWN_EVENT:
-'''
-#CSV
-with open('results.csv', 'w', newline='') as f:
+# CSV
+with open('results_must.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(title)
     writer.writerow(status)
@@ -89,6 +106,6 @@ with open('results.csv', 'w', newline='') as f:
     for row in total_matrix:
         writer.writerow(row)
 
-#CSV to xlsx
-read_file = pd.read_csv (r'results.csv')
-read_file.to_excel (r'results.xlsx', index = None, header=True)
+# CSV to xlsx
+read_file = pd.read_csv (r'results_must.csv')
+read_file.to_excel (r'results_must.xlsx', index = None, header=True)
