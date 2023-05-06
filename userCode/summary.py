2d57bb2a9c4e0bb37b0ef8273e4f4e5c5e2f50b2
francesca2311 <f.tirino@studenti.unisa.it>
2023-05-06 16:27:33 +0100

    add simulation_game

--- userCode/summary.py
+++ userCode/summary.py
@@ -7,14 +7,13 @@ INFRACTIONS = True
 SCORE = True
 SHUTDOWN_EVENT = True
 
-list_necess = []
 merged_dict = {}
 custom_dict = {}
 
 infractions_dict_new = {"collisions_layout":"Coll with Layout", "collisions_pedestrian":"Coll with Pedestrians", "collisions_vehicle":"Coll with Vehicles", 
                         "red_light":"Red light", "stop_infraction":"Stop", "outside_route_lanes":"Outside lanes", "min_speed_infractions":"Useless Decelerations", 
                         "yield_emergency_vehicles_infractions":"Yield emergency vehicles", "scenario_timeouts":"Scenario timeouts"}
-score_dict_new = {"score_composed":"Driving score", "score_route":"Route Complention", "score_penalty":"Infraction Penalty"}
+score_dict_new = {"score_composed":"Driving score", "score_route":"Route Complention", "score_penalty":"Infraction Penalty", "duration_game":"Simulation time (s)"}
 shutdown_event_dict_new = {"route_dev":"Route Deviation", "vehicle_blocked":"Vehicle Blocked", "route_timeout":"Route Timeout"}
 
 # Load the JSON file
@@ -42,12 +41,12 @@ for i in range(len(routes)):
 infractions_dict_comp = data['_checkpoint']['global_record']['infractions']
 infractions_dict = dict((k, infractions_dict_comp[k]) for k in list(infractions_dict_comp.keys())[:-3])
 values = [float(val) for val in list(infractions_dict.values())]
-
 # Percentages infractions
 total = sum(values)
 percentages = [val/total*100 for val in values]
 for key in infractions_dict:
     infractions_dict[key] = f"{round(percentages.pop(0),1):.1f}%"
+
 # Dict score global
 score_dict = data['_checkpoint']['global_record']['scores_mean']
 
@@ -55,15 +54,13 @@ score_dict = data['_checkpoint']['global_record']['scores_mean']
 shutdown_event_dict = dict((k, infractions_dict_comp[k]) for k in list(infractions_dict_comp.keys())[-3:])
 
 if INFRACTIONS:
-    list_necess.append(infractions_dict)
     merged_dict.update(infractions_dict)
     custom_dict.update(infractions_dict_new)
 if SCORE:
-    list_necess.append(score_dict)
     merged_dict.update(score_dict)
+    merged_dict.update({"duration_game":''})
     custom_dict.update(score_dict_new)
 if SHUTDOWN_EVENT:
-    list_necess.append(shutdown_event_dict)
     merged_dict.update(shutdown_event_dict)
     custom_dict.update(shutdown_event_dict_new)
 
@@ -83,9 +80,12 @@ for i in range(len(routes)):
                 total_matrix[j][i+2] = ''
             else:
                 total_matrix[j][i+2] = shutdown_event_value[0]
-        elif total_matrix[j,0] in score_dict:
+        elif total_matrix[j,0] in score_dict and total_matrix[j,0] != "duration_game":
             score_value = data['_checkpoint']['records'][i]['scores'][total_matrix[j,0]]
             total_matrix[j][i+2] = round(score_value,1)
+        elif total_matrix[j,0] == "duration_game":
+            duration_game = data['_checkpoint']['records'][i]['meta'][total_matrix[j,0]]
+            total_matrix[j][i+2] = round(duration_game,1)
         else:
             infraction_value = len(data['_checkpoint']['records'][i]['infractions'][total_matrix[j,0]])
             if infraction_value == 0:
