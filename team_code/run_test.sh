4ca349a6564d99789608272a396a3182da1fbd45
giosullutrone <giovisull@gmail.com>
2023-05-16 09:28:04 +0200

    Added overtake stoppping

--- team_code/run_test.sh
+++ team_code/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # export ROUTES=./routes_avddiem_exam.xml
 # export ROUTES=./route_controlling.xml
-export ROUTES=./second_scenario.xml
-# export ROUTES=./first.xml
+# export ROUTES=./second_scenario.xml
+export ROUTES=./first.xml
 # export ROUTES=./test.xml
 
 export REPETITIONS=1
